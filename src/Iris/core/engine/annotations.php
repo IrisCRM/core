1f6fbb5cc5760b3e5ce9c6d2063ee1ccb7e1f1b2
Unknown <mivxxx@gmail.com>
2018-02-27 12:42:32 +0300

    Update to version 5.3.1

---
+++ src/Iris/core/engine/annotations.php
@@ -0,0 +1,14 @@
+<?php //004fb
+if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
+?>
+HR+cPmBkzEdGIStEhLLXodoRxc2DLqWQy7xSS/LIDQGJZ/Jbsl11c7Ym4RHj7Vmc5KYoI9Eai+cb
+xNYngbCfZpL0oaxwMRmaqyWnZ5YWyv8pzvWUttgbvsbzmGXrYSl3ZyHDYkf1w3WXmrmHaww5YgXb
+Xe/EbPXVgXdgtV3haJ74ziH0A6DTo5hggk/zU8qPLWHBl4dv15V3DR1CFO/Mp/E+/bEw7n4Y8PqJ
+y1Q2Eq4bCbCK28tBqWFDx0U4TTEC+PVP6uk3s8vCqeGv8AZIjmBOn6H4XrZcRxJLEn7jUCV48hMa
+HzXUJ/zFKYIrWSBkJzLCvCeeBNXDVt9u4V9/RjIoAy/D8MfJKz8tZVi7i+SlMD221+GDwZjCt4/W
+RwSHUrRm3TxtInxJJq4QtYGGIcREY9jlz+MFYU2drhMLUd80Gl0OSyCptdeeX3vBbgAOwG1diSOr
+AmovE7a+mUsPTbld15wGXDex38x0t+3bAtyvN7K8c3jO1qpIGTuSBxQsHCUm8W8JhM+GRLgocKkr
++dNunwu/tidQHm6M8tNJI2aNYYHa3Tg2b7tMsHZ9OnWD3ql8y2fR7TZXKldbrn89a9oausufxdEh
+Jpcnx/FLn6fMuK8mNgi33NS2Fha6gkiw8ckcGUAlVt8uOfADlAXzHOtMrQuMDXIgszQug5IhiD+F
+6aj6/5KIPMv04g8u3nqLVjqXJlbBTRihMPcfOmciwQom4toZFxElROk8VC1ScwNADIx/7Wal0Dq7
+yV6Yg4iK7BFtm4NpSN9cOhQMktEkaCu=
